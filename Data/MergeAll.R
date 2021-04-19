b57560d8a1801cfa7c36893842557b78ad4f4293
FWyckmans <fwyckmans@gmail.com>
2021-04-19 14:10:05 +0200

    Updated MergeAll

--- Data/MergeAll.R
+++ Data/MergeAll.R
@@ -1,6 +1,6 @@
 ##### Total Merge
 # Initialization
-source("DMA_Init.R")
+source("DMG_Init.R")
 
 Datapath = "Data/Pavlovia/Processed/"
 Output_path = "Output/"
@@ -31,64 +31,64 @@ dRL2 <- dRL2%>%
 
 dTot <- cbind(dQ, dRL1[3:length(dRL1)], dRL2[3:length(dRL2)])
 
-##### Organize between Alcohol first and Neutral First
-# Alc First
-dAlF <- dTot%>%
-  filter(FirstVid == "Alcohol")
+##### Organize between Gambling first and Neutral First
+# Gambling First
+dGaF <- dTot%>%
+  filter(FirstVid == "Gambling")
 
-colnames(dAlF) <- sub("D1", "AfterAlc", colnames(dAlF))
-colnames(dAlF) <- sub("D2", "AfterNeutr", colnames(dAlF))
+colnames(dGaF) <- sub("D1", "AfterGamb", colnames(dGaF))
+colnames(dGaF) <- sub("D2", "AfterNeutr", colnames(dGaF))
 
-colnames(dAlF) <- sub("Craving1", "CravingBfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Craving2", "CravingAfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Craving3", "CravingAfAlc2", colnames(dAlF))
-colnames(dAlF) <- sub("Craving4", "CravingBfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Craving5", "CravingAfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Craving6", "CravingAfNeu2", colnames(dAlF))
+colnames(dGaF) <- sub("Craving1", "CravingBfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Craving2", "CravingAfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Craving3", "CravingAfGamb2", colnames(dGaF))
+colnames(dGaF) <- sub("Craving4", "CravingBfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Craving5", "CravingAfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Craving6", "CravingAfNeu2", colnames(dGaF))
 
-colnames(dAlF) <- sub("Resist1", "ResistBfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Resist2", "ResistAfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Resist3", "ResistAfAlc2", colnames(dAlF))
-colnames(dAlF) <- sub("Resist4", "ResistBfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Resist5", "ResistAfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Resist6", "ResistAfNeu2", colnames(dAlF))
+colnames(dGaF) <- sub("Resist1", "ResistBfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Resist2", "ResistAfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Resist3", "ResistAfGamb2", colnames(dGaF))
+colnames(dGaF) <- sub("Resist4", "ResistBfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Resist5", "ResistAfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Resist6", "ResistAfNeu2", colnames(dGaF))
 
-colnames(dAlF) <- sub("Stress1", "StressBfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Stress2", "StressAfAlc", colnames(dAlF))
-colnames(dAlF) <- sub("Stress3", "StressAfAlc2", colnames(dAlF))
-colnames(dAlF) <- sub("Stress4", "StressBfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Stress5", "StressAfNeu", colnames(dAlF))
-colnames(dAlF) <- sub("Stress6", "StressAfNeu2", colnames(dAlF))
+colnames(dGaF) <- sub("Stress1", "StressBfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Stress2", "StressAfGamb", colnames(dGaF))
+colnames(dGaF) <- sub("Stress3", "StressAfGamb2", colnames(dGaF))
+colnames(dGaF) <- sub("Stress4", "StressBfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Stress5", "StressAfNeu", colnames(dGaF))
+colnames(dGaF) <- sub("Stress6", "StressAfNeu2", colnames(dGaF))
 
 # Neutral First
 dNeF <- dTot%>%
   filter(FirstVid == "Neutral")
 
 colnames(dNeF) <- sub("D1", "AfterNeutr", colnames(dNeF))
-colnames(dNeF) <- sub("D2", "AfterAlc", colnames(dNeF))
+colnames(dNeF) <- sub("D2", "AfterGamb", colnames(dNeF))
 
 colnames(dNeF) <- sub("Craving1", "CravingBfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Craving2", "CravingAfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Craving3", "CravingAfNeu2", colnames(dNeF))
-colnames(dNeF) <- sub("Craving4", "CravingBfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Craving5", "CravingAfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Craving6", "CravingAfAlc2", colnames(dNeF))
+colnames(dNeF) <- sub("Craving4", "CravingBfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Craving5", "CravingAfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Craving6", "CravingAfGamb2", colnames(dNeF))
 
 colnames(dNeF) <- sub("Resist1", "ResistBfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Resist2", "ResistAfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Resist3", "ResistAfNeu2", colnames(dNeF))
-colnames(dNeF) <- sub("Resist4", "ResistBfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Resist5", "ResistAfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Resist6", "ResistAfAlc2", colnames(dNeF))
+colnames(dNeF) <- sub("Resist4", "ResistBfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Resist5", "ResistAfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Resist6", "ResistAfGamb2", colnames(dNeF))
 
 colnames(dNeF) <- sub("Stress1", "StressBfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Stress2", "StressAfNeu", colnames(dNeF))
 colnames(dNeF) <- sub("Stress3", "StressAfNeu2", colnames(dNeF))
-colnames(dNeF) <- sub("Stress4", "StressBfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Stress5", "StressAfAlc", colnames(dNeF))
-colnames(dNeF) <- sub("Stress6", "StressAfAlc2", colnames(dNeF))
+colnames(dNeF) <- sub("Stress4", "StressBfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Stress5", "StressAfGamb", colnames(dNeF))
+colnames(dNeF) <- sub("Stress6", "StressAfGamb2", colnames(dNeF))
 
-dTot <- rbind(dAlF, dNeF)
+dTot <- rbind(dGaF, dNeF)
 
 ##### Export
 write.table(dTot, paste0(Output_path, "dTot.txt"), col.names = T, row.names = F, sep = "\t", dec = ".")
