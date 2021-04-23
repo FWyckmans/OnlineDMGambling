9b4f25674f9f148cc64b1a9be3271e457457228d
FWyckmans <fwyckmans@gmail.com>
2021-04-23 14:56:39 +0200

    Updatd logistic regressions for gambling

---
+++ Analyses/MainLogisticRegression.R
@@ -0,0 +1,51 @@
+# Computation
+##### Initialization
+source("DMG_Init.R")
+Datapath = "Output/"
+
+##### DatFrame
+# Import
+dRegLog <- read.delim(paste0(Datapath, "dRegLog.txt"))
+dRegLogGamb <- filter(dRegLog, Time == "AfterGambling")
+dRegLogNeu <- filter(dRegLog, Time == "AfterNeutral")
+
+##### Logistic Regressions
+## With Time
+RL <- glmer(Stay ~ PrReward*PrTransition*Time*Grp + (PrReward*PrTransition*Time|subjID),
+            family = binomial, data = dRegLog, control = glmerControl(optimizer = "bobyqa"),
+            nAGQ = 1)
+
+summary(RL)
+exp(coef(RL))
+pred = predict(RL)
+
+##### Evaluation
+Conf_Mat <- ConfMat(d = dRegLog, pred = pred, DV = "Stay")
+RL_Metric <- RLMetrics(Conf_Mat, pred = pred, T)
+
+## Without Time
+# After Gambling
+RLGamb <- glmer(Stay ~ PrReward*PrTransition*Grp + (PrReward*PrTransition|subjID),
+            family = binomial, data = dRegLogGamb, control = glmerControl(optimizer = "bobyqa"),
+            nAGQ = 1)
+
+summary(RLGamb)
+exp(coef(RLGamb))
+pred = predict(RLGamb)
+
+# Evaluation
+Conf_MatGamb <- ConfMat(d = dRegLogGamb, pred = predict(RLGamb), DV = "Stay")
+RL_MetricGamb <- RLMetrics(Conf_MatGamb, pred = predict(RLGamb))
+
+# After Neutral
+RLNeu <- glmer(Stay ~ PrReward*PrTransition*Alc + (PrReward*PrTransition|subjID),
+               family = binomial, data = dRegLogNeu, control = glmerControl(optimizer = "bobyqa"),
+               nAGQ = 1)
+
+summary(RLNeu)
+exp(coef(RLNeu))
+pred = predict(RLNeu)
+
+# Evaluation
+Conf_MatNeu <- ConfMat(d = dRegLogNeu, pred = predict(RLNeu), DV = "Stay")
+RL_MetricNeu <- RLMetrics(Conf_MatNeu)
