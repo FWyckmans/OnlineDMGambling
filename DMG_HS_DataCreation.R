371ca7ac97ba88404588b2ef8435d5060ca969d8
FWyckmans <fwyckmans@gmail.com>
2022-05-17 16:27:43 +0200

    Updated HS

--- DMG_HS_DataCreation.R
+++ DMG_HS_DataCreation.R
@@ -13,9 +13,10 @@ source("AdditionalInfo/MailList/MailCheck.R")
 
 source("DataPreparation/4.Anonymization.R")
 
-# Launch from here if you do not have access to raw data
+# Launch from here if you do not have access to raw data and want to do the computations by yourselves
 source("DataPreparation/5.Computations.R")
 
+# Launch from here if you do not want to redo the computations and do not have all the raw data
 source("DataPreparation/6.AdditComp.R")
 
 source("DataPreparation/7.PrepForRegLog.R")
